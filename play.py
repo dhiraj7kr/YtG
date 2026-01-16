b550704ff96235edcf43ed9b4be7cd462d8563b0
Dhiraj Kumar <dhiraj7kr@gmail.com>
2026-01-17 01:27:12 +0530

    Added

---
+++ play.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+url = "https://raw.githack.com/dhiraj7kr/YtG/main/How%20to%20install%20Flutter%20on%20Windows%202025%20_%20Setup%20Android%20Studio%20for%20Flutter%20Step%20by%20Step.mp4"
+
+webbrowser.open(url)
